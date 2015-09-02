b807bd0f8d5e68b07fd8df88f93084f3f9a8499b
xy515258 <cbh515258@gmail.com>
2015-09-02 11:03:48 +0800

    snow_flake_2D
    The changed file, input file and description of snow_flake example for
    MOOSE.

---
+++ CoefDiffusion.h
@@ -0,0 +1,35 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#ifndef COEFDIFFUSION_H
+#define COEFDIFFUSION_H
+
+#include "Kernel.h"
+#include "Function.h"
+
+//Forward Declarations
+class CoefDiffusion;
+
+template<>
+InputParameters validParams<CoefDiffusion>();
+
+class CoefDiffusion : public Kernel
+{
+public:
+
+  CoefDiffusion(const std::string & name, InputParameters parameters);
+
+protected:
+  virtual Real computeQpResidual();
+
+  virtual Real computeQpJacobian();
+
+private:
+  Real _coef;
+  Function * const _func;
+
+};
+#endif //COEFDIFFUSION_H
